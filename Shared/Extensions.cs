c21fe0bf8ad0a75109a3099912d81bbccaa68351
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2021-03-15 18:20:18 +0100

    Rewrite and add UncensorSelector and AccessoryClothes support (#1)
    * Rewrite to be simpler and allow using with AccessoryClothes plugin
    
    * Add UncensorSelector support
    
    * Fix not finding some bones when implanting uncensors

--- Shared/Extensions.cs
+++ Shared/Extensions.cs
@@ -1,23 +1,61 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using HarmonyLib;
 using UnityEngine;
 
 namespace ModBoneImplantor
 {
     internal static class Extensions
     {
-
-        public static void Destroy(this GameObject self, bool useDestroyImmediate = false, bool detachParent = false)
+        public static void FancyDestroy(this GameObject self, bool useDestroyImmediate = false, bool detachParent = false)
         {
             if (self == null) return;
             if (detachParent) self.transform.SetParent(null);
             if (useDestroyImmediate) Object.DestroyImmediate(self);
             else Object.Destroy(self);
         }
-        
-        public static bool IsNullOrEmpty(this ICollection self)
+
+        public static string GetFullPath(this GameObject self)
         {
-            return self == null || self.Count == 0;
+            if (self == null) return string.Empty;
+
+            var result = new StringBuilder();
+            var first = true;
+            var transform = self.transform;
+            while (transform != null)
+            {
+                if (first) first = false;
+                else result.Insert(0, "/");
+                result.Insert(0, transform.gameObject.name);
+                transform = transform.parent;
+            }
+            return result.ToString();
+        }
+
+        public static string GetFullPath(this Component self)
+        {
+            if (self == null) return string.Empty;
+            return $"{self.gameObject.GetFullPath()} [{self.GetType().Name}]";
+        }
+
+        public static Dictionary<string, GameObject> GetBodyBoneDict(this ChaControl ctrl)
+        {
+            var aaw = Traverse.Create(ctrl).Field<AssignedAnotherWeights>("aaWeightsBody").Value;
+            return aaw.dictBone;
+        }
+
+        public static Dictionary<string, GameObject> GetBoneDict(this SkinnedMeshRenderer dst)
+        {
+            return dst.bones.Where(x => x != null).ToDictionary(x => x.name, x => x.gameObject);
+        }
+
+        public static Transform GetTopmostParent(this Component src)
+        {
+            var topmostParent = src.transform;
+            while (topmostParent.parent)
+                topmostParent = topmostParent.parent;
+            return topmostParent;
         }
     }
 }
--- Shared/ModBoneImplantor.cs
+++ Shared/ModBoneImplantor.cs
@@ -1,20 +1,68 @@
-﻿using BepInEx;
+﻿using System.Collections.Generic;
+using BepInEx;
 using BepInEx.Logging;
+using HarmonyLib;
+using UnityEngine;
 
 namespace ModBoneImplantor
 {
     [BepInPlugin(GUID, "Mod Bone Implantor", Version)]
-    public class ModBoneImplantor : BaseUnityPlugin
+    [BepInDependency("com.deathweasel.bepinex.uncensorselector", BepInDependency.DependencyFlags.SoftDependency)]
+    public partial class ModBoneImplantor : BaseUnityPlugin
     {
         public const string GUID = "com.rclcircuit.bepinex.modboneimplantor";
-        public const string Version = "0.2.4";
+        public const string Version = "1.0";
 
         internal static new ManualLogSource Logger;
 
         private void Awake()
         {
             Logger = base.Logger;
-            Hooks.InstallHooks();
+
+            var hi = new Harmony(GUID);
+            AssignedAnotherWeightsHooks.InstallHooks(hi);
+            UncensorSelectorSupport.InstallHooks(hi);
+        }
+
+        private static bool TryImplantBones(GameObject loadedObj, Dictionary<string, GameObject> existingBoneDict,
+            out List<Transform> implantedBones, out List<DynamicBoneCollider> implantedColliders)
+        {
+            implantedColliders = null;
+            implantedBones = null;
+
+            var implants = loadedObj.GetComponentsInChildren<BoneImplantProcess>();
+            if (implants.Length == 0) return false;
+
+            // Implant extra bones from the object into base body skeleton based on BoneImplantProcess components attached to the object
+            implantedBones = new List<Transform>(implants.Length);
+            implantedColliders = new List<DynamicBoneCollider>(implants.Length);
+            foreach (var implantInfo in implants)
+            {
+                var boneToImplant = implantInfo.trfSrc;
+                var targetParentBone = implantInfo.trfDst;
+                if (boneToImplant == null || targetParentBone == null || boneToImplant == targetParentBone)
+                {
+                    Logger.Log(LogLevel.Error, $"Your BoneImplantProcess is invalid. trfSrc is {(boneToImplant != null ? boneToImplant.name : "NULL")} and trfDst is {(targetParentBone != null ? targetParentBone.name : "NULL")}.");
+                    Logger.Log(LogLevel.Error, "1) You must specify both trfSrc and trfDst.");
+                    Logger.Log(LogLevel.Error, "2) trfSrc must be different from trfDst.");
+                }
+                // Find a bone in the body skeleton with the same name as the trfDst bone in the BoneImplantProcess
+                else if (existingBoneDict.TryGetValue(targetParentBone.name, out var targetDstObj))
+                {
+                    boneToImplant.SetParent(targetDstObj.transform, false);
+                    implantedBones.AddRange(boneToImplant.GetComponentsInChildren<Transform>(true));
+                    implantedColliders.AddRange(boneToImplant.GetComponentsInChildren<DynamicBoneCollider>(true));
+                }
+                else
+                {
+                    Logger.Log(LogLevel.Error, $"Your BoneImplantProcess is invalid: trfDst wasn't found in the body bones. trfDst is {targetParentBone.name}.");
+                    Logger.Log(LogLevel.Error, "trfDst must be a bone stored in the same structure as official body skeleton (name must match).");
+                    Logger.Log(LogLevel.Error, "trfDst cannot be set to original bone or placeholder object such as cf_o_root.");
+                }
+            }
+
+            Logger.LogDebug($"Found {implants.Length} instances of BoneImplantProcess. In total {implantedBones.Count} bones were implanted.");
+            return true;
         }
     }
 }
